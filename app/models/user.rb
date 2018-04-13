a334ed30991dc94eeade4db1efdbdc92495336d3
anandbait <anand.bait@gmail.com>
2018-04-13 23:20:00 +0530

    Friends functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   has_many :favourite_genres
 
   validates :username, uniqueness: true, format: { with: /\A\w*\z/, message: "only allows letters" }
+
+  def friend_request_received?(friend)
+    HasFriendship::Friendship.find_relation(self, friend, status: 1).any?
+  end
+
+  def friend_request_sent?(friend)
+    HasFriendship::Friendship.find_relation(self, friend, status: 0).any?
+  end
 end
